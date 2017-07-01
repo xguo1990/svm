e49b91ec6cd1a9dddd20ab8676698c17745f348f
Xiang Guo <xguo1990@gmail.com>
2017-07-01 18:25:07 -0400

    Realize SVM with default setting.

---
+++ svm_author_id.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+from sklearn.svm import SVC
+#clf = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+#    decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
+#    max_iter=-1, probability=False, random_state=None, shrinking=True,
+#    tol=0.001, verbose=False)
+clf = SVC()
+clf.fit(features_train, labels_train) 
+
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test,pred)
+
+#########################################################
+### your code goes here ###
+
+#########################################################
+
+
